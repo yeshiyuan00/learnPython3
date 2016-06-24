636b2960dfe405fa371fd89225ecbbc1023c11e0
yeshiyuan00 <yeshiyuan00@gmail.com>
2016-06-24 16:20:32 +0800

    asyncio

---
+++ asyncio/async_hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio
+import threading
+
+@asyncio.coroutine
+def hello():
+	print("Hello World!")
+	# 异步调用asyncio.sleep(1)
+	r = yield from asyncio.sleep(1)
+	print("Hello again!")
+
+# 获取EventLoop：
+loop = asyncio.get_event_loop()
+# 执行coroutine
+loop.run_until_complete(hello())
+# loop.close()
+
+@asyncio.coroutine
+def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    yield from asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
